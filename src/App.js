89b7a28d0e6f5521c68062411d3f6cf5df7f793c
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-04-11 18:28:08 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+          <h1>CS 230L</h1>
+          <h2>Section - 002</h2>
+          <p>WVU ID: 800386690</p>
+          <p>Hi I am James Kmetz</p>
+    </div>
+  );
+}
+
+export default App;
